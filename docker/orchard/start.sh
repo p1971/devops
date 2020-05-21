68c5e17b517d47e463998aef8975f634fa472c25
p1971 <pete@madhippy.com>
2020-05-21 12:05:57 +0200

    Init commit of docker files

---
+++ docker/orchard/start.sh
@@ -0,0 +1,1 @@
+docker run --restart=unless-stopped --name orchardcms -p 8089:80 orchardproject/orchardcore-cms-linux:latest
---
+++ docker/rabbitmq/start.sh
@@ -0,0 +1,1 @@
+docker run -d --hostname sharon --name rabbittest -p 15672:15672 rabbitmq:3-management
